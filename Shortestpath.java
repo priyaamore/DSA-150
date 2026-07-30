7d9488ca14130e971a5b8ac7513f4ff12d2063da
Priya  More <priyamore8446@gmail.com>
2026-07-30 23:39:43 +0530

    Update Shortestpath.java

--- Shortestpath.java
+++ Shortestpath.java
@@ -1,1 +1,33 @@
+public class Shortestpath {
+
+    public static void Shortestdistance(String str){
+        int y=0;
+        int x=0;
+       
+        for(int i=0;i<str.length();i++){
+            // String str ="NSEWWWNSW";
+            int dir = str.charAt(i);
+             
+            
+            if(dir == 'N'){
+                y++;
+            }else if(dir == 'S'){
+                y--;
+
+            }else if(dir == 'E'){
+                x++;
+
+            }else{
+                x--;
+            }
+        }
+        double distance = (x*x + y*y);
+        System.out.println(distance);
+    }
+    public static void main(String[]args){
+        String str ="NSEWWWNSW";
+        Shortestdistance(str);
+    }
+    
+}
 
