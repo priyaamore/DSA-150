e810dcaf80c93be66ffa6597a89e6f6af8abc472
Priya  More <priyamore8446@gmail.com>
2026-08-18 21:41:45 +0530

    Add Next Greater Element program
    Implemented a program to find the next greater element for each element in an array using a stack.

---
+++ Next_greater.java
@@ -0,0 +1,29 @@
+package Stack;
+import java.util.*;
+
+public class Next_greater {
+
+
+    public static void main(String[]args){
+        int arr[] = {5,7,8,6,0,1};
+        Stack<Integer> s = new Stack<>();
+       int  nextGreater[] = new int[arr.length];
+
+       for(int i= arr.length-1; i>=0; i--){
+        // while loop
+        while(!s.isEmpty() && arr[s.peek()] <= arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nextGreater[i] = -1;
+            }else{
+                nextGreater[i] = arr[s.peek()];
+            }
+            s.push(i);
+       }
+        for(int i=0; i<nextGreater.length; i++){
+            System.out.print(nextGreater[i]+" ");
+        }
+    }
+    
+}
