193ccc1e68b2c27d0e5d72ae3e58b474ffd45668
Priya  More <priyamore8446@gmail.com>
2026-08-23 17:29:51 +0530

    Add queue implementation using two stacks
    Implement a queue using two stacks with add, remove, and peek methods.

---
+++ S2.java
@@ -0,0 +1,59 @@
+package Queue;
+
+import java.util.*;
+
+public class Que_using_2stack{
+
+    public static class Queue{
+        static  Stack<Integer> s1 = new Stack<>();
+        static Stack<Integer> s2 = new Stack<>();
+
+        // isEmpty
+        public static boolean isEmpty(){
+            return s1.isEmpty();
+
+        }
+        // add
+        public static void add(int data){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+
+            s2.push(data);
+
+            while(!s2.isEmpty()){
+                s1.push(s2.pop());
+            }
+        }
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("Queue is Empty");
+                return -1;
+
+            }
+            return s1.pop();
+        }
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("Queue is Empty");
+                return -1;
+
+            }
+            return s1.peek();
+        }
+    }
+    public static void main(String[]args){
+        Queue q = new Queue();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while(!q.isEmpty()){
+
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+    
+}
